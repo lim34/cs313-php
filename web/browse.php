73d7c57cc350668f2ee2e0b7d626af2c41c2c302
Lim <lim12001@byui.edu>
2018-05-12 10:35:56 -0600

    adding welcome, browse, cart, and check out page

---
+++ web/browse.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<?php 
+session_start();
+?>
+
+<html>
+<head>
+  <link rel="stylesheet" href="style.css">
+  <title>Browse Page</title>
+</head>
+<body>
+
+  <h1>Welcome to DJ's Thrift Store!</h1>
+  <h3>
+    Are you ready to pop some tags?
+  </h3><p>
+    Items for purchase: 
+    </p>
+  <form action="header.php" method="POST" onSubmit="window.location.reload()">
+    <ul>
+      
+      <li><input type="checkbox" name="items[]" value="Long Coat"> Long Coat (Grandpa Style) - $5.00 </li>
+      <li><input type="checkbox" name="items[]" value="Old Hat"> Old Hat - $7.00 </li>
+      <li><input type="checkbox" name="items[]" value="Golf Clubs"> A set of Golf Clubs - $13.00 </li>
+      <li><input type="checkbox" name="items[]" value="Ray Bands"> Ray Bands - $3.00</li>
+      <li><input type="checkbox" name="items[]" value="Golden Calf"> Golden Calf - $39.00</li>
+      <li><input type="checkbox" name="items[]" value="Super Old Chinese Ginseng"> Super Old Chinese Ginseng - $199.00 </li>
+      </ul>
+    
+    <input type="submit">
+    </button>
+</form>
+<form action="header2.php" method="GET">
+<input type="submit" value="View My Cart">
+</form>
+</body>
+</html>
